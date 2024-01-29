24f48ce48ea0bd8107c2775d6bd090d29a4a19c7
장영주 <corydalisss@gmail.com>
2024-01-29 22:20:03 +0900

    Update TodoList.js

--- dashboard/src/components/TodoList.js
+++ dashboard/src/components/TodoList.js
@@ -3,11 +3,13 @@ import React, { useState } from "react";
 export const TodoList = () => {
   const [list, setList] = useState("");
   const [addList, setAddList] = useState([]);
+  const [line, setLine] = useState(false);
 
   const onChange = (e) => {
     const ee = e.target.value;
     setList(ee);
   };
+
   const onSubmit = (e) => {
     e.preventDefault();
     if (addList === "") {
@@ -15,7 +17,30 @@ export const TodoList = () => {
     }
     console.log(addList);
     setAddList((zz) => [list, ...zz]);
+    // setAddList((zz) => [{ task: list, completed: false }, ...zz]);
+
     setList("");
+  };
+
+  const onCheckedElement = (checked, item) => {
+    if (checked) {
+      setAddList([...addList, item]);
+    } else if (!checked) {
+      setAddList(addList.filter((el) => el !== item));
+    }
+  };
+
+  const handleCheckboxChange = (index) => {
+    const newList = addList.map((item, i) =>
+      i === index ? { ...item, completed: !item.completed } : item
+    );
+    setAddList(newList);
+  };
+
+  const removeit = (index) => {
+    const newList = addList.filter((item, i) => i !== index);
+    console.log(newList);
+    setAddList(newList);
   };
 
   return (
@@ -27,9 +52,24 @@ export const TodoList = () => {
           {addList.map((item, key) => {
             return (
               <li key={key}>
-                <input type="checkbox" value={item} id={item} />{" "}
+                <input
+                  type="checkbox"
+                  value={item}
+                  id={item}
+                  onChange={(e) => {
+                    onCheckedElement(e.target.checked, e.target.value);
+                  }}
+                  checked={addList.includes(item) ? true : false}
+                />{" "}
                 <label for={item}>{item}</label>
-                <button> 다했슈 </button>
+                <button
+                  onClick={() => {
+                    removeit();
+                  }}
+                >
+                  {" "}
+                  다했슈{" "}
+                </button>
               </li>
             );
           })}
