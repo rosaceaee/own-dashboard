ddd3fc536cc0bf51026841a248f2fed28123719a
장영주 <corydalisss@gmail.com>
2024-02-29 14:19:05 +0900

    Update TodoContainer.js

--- dashboard/src/components/TodoContainer.js
+++ dashboard/src/components/TodoContainer.js
@@ -7,7 +7,7 @@ const TodoContainer = ({ container }) => {
   const [inputText, setInputText] = useState("");
 
   useEffect(() => {
-    const localStorageKey = `todoData-${id}`;
+    const localStorageKey = `${id}`;
     const storedData = JSON.parse(localStorage.getItem(localStorageKey));
     if (storedData) {
       setTodos(storedData);
@@ -46,6 +46,19 @@ const TodoContainer = ({ container }) => {
     }
   };
 
+  const remove = (i) => {
+    const updatedList = [...todos];
+    updatedList.splice(i, 1);
+    setTodos(updatedList);
+
+    // 로컬 스토리지에 저장된 기존 데이터를 불러와서 업데이트된 리스트를 추가
+    const localStorageKey = `todoData-${id}`;
+    const storedData = JSON.parse(localStorage.getItem(localStorageKey)) || [];
+    const updatedData = [...storedData];
+    updatedData.splice(i, 1);
+    localStorage.setItem(localStorageKey, JSON.stringify(updatedData));
+  };
+
   return (
     <>
       <hr />
@@ -62,7 +75,9 @@ const TodoContainer = ({ container }) => {
         </div>
         <ul>
           {todos.map((todo, index) => (
-            <li key={index}>{todo}</li>
+            <li key={index}>
+              {todo} <p onClick={remove}>remove</p>
+            </li>
           ))}
         </ul>
       </div>
