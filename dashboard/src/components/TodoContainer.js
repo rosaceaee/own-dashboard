abaac176d58d7254c7f5cf789e50e0a207dea33a
장영주 <corydalisss@gmail.com>
2024-03-03 19:30:29 +0900

    update remove btn
    error fixed: remove each li in order

--- dashboard/src/components/TodoContainer.js
+++ dashboard/src/components/TodoContainer.js
@@ -1,11 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 
 const TodoContainer = ({ container }) => {
   const { id, name } = container;
 
   const [todos, setTodos] = useState([]);
   const [inputText, setInputText] = useState("");
-
+  const [throu, Setthrou] = useState(false);
   useEffect(() => {
     const localStorageKey = `${id}`;
     const storedData = JSON.parse(localStorage.getItem(localStorageKey));
@@ -17,18 +17,6 @@ const TodoContainer = ({ container }) => {
   const handleInputChange = (e) => {
     setInputText(e.target.value);
   };
-  {
-    /*
-  const handleAddTodo = () => {
-    if (inputText.trim() !== "") {
-      const updatedList = [...todos, inputText];
-      setTodos(updatedList);
-      const localStorageKey = `${id}`;
-      localStorage.setItem(localStorageKey, JSON.stringify(setTodos(updatedList)));
-      setInputText("");
-    }
-  };*/
-  }
 
   const handleAddTodo = () => {
     if (inputText.trim() !== "") {
@@ -36,7 +24,7 @@ const TodoContainer = ({ container }) => {
       setTodos(updatedList);
 
       // 로컬 스토리지에 저장된 기존 데이터를 불러와서 업데이트된 리스트를 추가
-      const localStorageKey = `todoData-${id}`;
+      const localStorageKey = `${id}`;
       const storedData =
         JSON.parse(localStorage.getItem(localStorageKey)) || [];
       const updatedData = [...storedData, inputText];
@@ -52,34 +40,36 @@ const TodoContainer = ({ container }) => {
     setTodos(updatedList);
 
     // 로컬 스토리지에 저장된 기존 데이터를 불러와서 업데이트된 리스트를 추가
-    const localStorageKey = `todoData-${id}`;
+    const localStorageKey = `${id}`;
     const storedData = JSON.parse(localStorage.getItem(localStorageKey)) || [];
     const updatedData = [...storedData];
     updatedData.splice(i, 1);
     localStorage.setItem(localStorageKey, JSON.stringify(updatedData));
   };
 
+  const eachChk = (chk) => {};
+
   return (
     <>
-      <hr />
-      <div>
-        <h2>{name}</h2>
-        <div>
+      <div className="todo-container">
+        <h2 className="repo-header">{name}</h2>
+        <ul>
+          {todos.map((todo, index) => (
+            <li key={index}>
+              <span>{todo}</span>
+              <p onClick={() => remove(index)}>remove</p>{" "}
+            </li>
+          ))}
+        </ul>
+        <div className="inputwrap">
           <input
             type="text"
             value={inputText}
             onChange={handleInputChange}
             placeholder="추가"
           />
-          <button onClick={handleAddTodo}>Add</button>
+          <button onClick={handleAddTodo}>등록</button>
         </div>
-        <ul>
-          {todos.map((todo, index) => (
-            <li key={index}>
-              {todo} <p onClick={remove}>remove</p>
-            </li>
-          ))}
-        </ul>
       </div>
     </>
   );
